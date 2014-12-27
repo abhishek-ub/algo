92cf6ff5d5deb8586ba12d845bc1a76f8be0c936
abhishek-ub <a35@buffalo.edu>
2014-12-27 16:22:29 -0500

    BST

---
+++ BinarySearchTree.java
@@ -0,0 +1,169 @@
+package zillow;
+
+public class BinarySearchTree {
+
+	private Node root;
+	private StringBuilder inorder;
+	
+	public static class Node{
+		public Integer data;
+		public Node left;
+		public Node right;
+		
+		public String getData(){
+			return Integer.toString(data);
+		}
+	}
+	
+	public BinarySearchTree(Integer rootdata){
+		root=new Node();	
+		root.data=rootdata;
+	}
+	
+	public void insert(Integer data){
+		Node temp=root;
+		Node prev=temp;
+		int l=0;
+		while(temp!=null){
+			if(data<=temp.data){
+				prev=temp;
+				temp=temp.left;
+				l=1;
+			}else{
+				prev=temp;
+				temp=temp.right;
+				l=0;
+			}
+		}
+		if(l==1){
+			prev.left=new Node();
+			prev.left.data=data;
+		}else{
+			prev.right=new Node();
+			prev.right.data=data;
+		}
+		
+	}
+	/*
+	 * Method deletes the first occurrence of element if found and
+	 * moves the right child in its place
+	 * @param element to be deleted
+	 */
+	public boolean delete(Integer data){
+		if(data==root.data){
+			if(root.left!=null){
+				Node left=root.left;
+				root=root.right;
+				Node temp=root;
+				Node prev=temp;
+				while(temp!=null){
+					prev=temp;
+					temp=temp.left;
+				}
+				prev.left=left;
+				return true;
+			}else{
+				root=root.right;
+				return true;
+			}
+		}else{
+			Node temp=root;
+			Node prev=temp;
+			int l=0;
+			while(temp!=null){
+				if(data<temp.data){
+					prev=temp;
+					temp=temp.left;
+					l=1;
+				}else if(data>temp.data){
+					prev=temp;
+					temp=temp.right;
+					l=0;
+				}else{
+					if(temp.right!=null){
+						Node left=temp.left;
+						temp=temp.right;
+						Node t=temp;
+						Node p=t;
+						while(t!=null){
+							p=t;
+							t=t.left;
+						}
+						p.left=left;
+					}else{
+						temp=temp.left;
+					}
+					
+					if(l==1){
+						prev.left=temp;
+					}else{
+						prev.right=temp;
+					}
+					
+					return true;
+				}
+			}
+		}
+		
+		System.out.println("Element Not found");
+		return false;
+	}
+	/*
+	 * Method to return inOrder traversal of 
+	 * Tree as string representation
+	 * 
+	 */
+	
+	public String inOrder(){
+		this.inorder=new StringBuilder();
+		inOrder(root);
+		return inorder.toString();
+	}
+	
+	
+	private void inOrder(Node head){
+		if(head==null)return;
+		inOrder(head.left);
+		inorder.append(head.getData()+" ");
+		inOrder(head.right);
+		
+	}
+	
+	
+	public Node getRoot(){
+		return root;
+	}
+	/*
+	 * This can potentially make tree non Binary search
+	 * Implementation should ensure the root being set is binary
+	 * 
+	 */
+	public void setRoot(Node n){
+		root=n;
+	}
+	
+	/*
+	 * Method to return minimum depth of tree 
+	 */
+	public int minHeight(){
+		return minHeight(root);
+	}
+	
+	private int minHeight(Node n){
+		if(n==null)return 0;
+		
+		return 1+Math.min(minHeight(n.left),minHeight(n.right));
+	}
+	
+	/*
+	 * Method to return maximum depth of tree 
+	 */
+	public int maxHeight(){
+		return maxHeight(root);
+	}
+	private int maxHeight(Node n){
+		if(n==null)return 0;
+		
+		return 1+Math.max(maxHeight(n.left),maxHeight(n.right));
+	}
+}
---
+++ TestBTree.java
@@ -0,0 +1,65 @@
+package zillow;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import zillow.BinarySearchTree.Node;
+
+public class TestBTree {
+
+	@Test
+	public void test() {
+		
+		BinarySearchTree bt=new BinarySearchTree(2);
+		bt.insert(1);
+		bt.insert(4);
+		bt.insert(3);
+		bt.insert(8);
+		bt.insert(10);
+		bt.insert(5);
+		bt.insert(-3);
+		bt.insert(-4);
+		bt.insert(-1);
+		System.out.println(bt.inOrder());
+		if(!bt.delete(-3))System.out.println("Failed to delete");
+		System.out.println(bt.inOrder());
+		assertTrue(bt.inOrder().contains("-4 -1 1 2 3 4 5 8 10"));
+		
+		int [] arr={1,2,3,4,5,6,7,8,9,10,11,12};
+		BinarySearchTree minBST=minimalBST(arr);
+		System.out.println(minBST.inOrder());
+		System.out.println(minBST.minHeight());
+		System.out.println(minBST.maxHeight());
+		assertEquals(minBST.minHeight(), 3);
+		assertEquals(minBST.maxHeight(), 4);
+	}
+	
+	/*
+	 * Given an sorted array
+	 * Method returns a balances B tree
+	 */
+	public BinarySearchTree minimalBST(int[] arr){
+		if(arr.length==0){
+			return null;
+		}else{
+			BinarySearchTree bt=new BinarySearchTree(arr[(0+arr.length)/2]);
+			bt.setRoot(minimalBST(arr,0,arr.length-1));
+			return bt;
+		}
+	}
+	
+	private Node minimalBST(int[] arr,int start,int end){
+		if(start>end) return null;
+		int mid=(start+end)/2;
+		Node bnd=new Node();
+		bnd.data=arr[mid];
+		bnd.left=minimalBST(arr,start,mid-1);
+		bnd.right=minimalBST(arr,mid+1,end);
+		return bnd;
+	}
+
+	
+	
+	
+}
